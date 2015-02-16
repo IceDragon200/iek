e94e954c66ab5fc9904b4a6781ad18d3b47c316a
Corey Powell <mistdragon100@gmail.com>
2015-02-16 00:29:05 -0500

    Updated sapling package

--- lib/sapling/sapling.rb
+++ lib/sapling/sapling.rb
@@ -1,1 +1,13 @@
-$simport.r('sapling', '1.0.0', 'Provides a event server for other scripts')
+$simport.r('sapling', '1.0.0', 'Provides a character + battler binding API')
+
+module Sapling
+  # CharacterBinding will implement the Character hosting API
+  module CharacterBinding
+    attr_accessor :character
+  end
+
+  # BattlerBinding will implement the Battler hosting API
+  module BattlerBinding
+    attr_accessor :battler
+  end
+end

e53a82078f1fb32a534b69b402344bb92d711aa7
Corey Powell <mistdragon100@gmail.com>
2016-01-06 21:46:11 -0500

    iex3: Split vx_patches into smaller files

--- lib/iex3/vx_patches/window_item.rb
+++ lib/iex3/vx_patches/window_item.rb
@@ -7,18 +7,10 @@ class Window_Item < Window_Selectable
   #     height : window height
   #--------------------------------------------------------------------------
   def initialize(x, y, width, height)
-    super(x, y, width, height)
+    super x, y, width, height
     @data = []
     @column_max = 2
     @index = 0
     refresh
-  end
-end
-
-class Scene_Skill
-  alias :terminate_wo_viewport :terminate
-  def terminate
-    terminate_wo_viewport
-    @viewport.dispose
   end
 end
